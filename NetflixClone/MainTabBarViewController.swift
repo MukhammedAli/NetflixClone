1b41616749425fa768a3605154a9e97cf4aab810
MukhammedAli <muhammedalikhamzayev@gmail.com>
2023-02-04 14:16:39 +0600

    UITabBarView initialized with ViewControllers

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -11,7 +11,27 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DonwloadsViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        vc1.title = "Home" 
+        vc2.title = "Coming soon"
+        vc3.title = "Top search"
+        vc4.title = "Downloads"
+        
+       // tabBar.tintColor = .label
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        
     }
 
 
---
+++ NetflixClone/ViewControllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  NetflixClone
+//
+//  Created by  Mukhammed Ali Khamzayev on 02.02.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
